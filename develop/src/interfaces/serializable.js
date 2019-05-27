b7be0a11e541c7ddcb183744f7b18e6aac2f4f22
dip686 <dip686@gmail.com>
2019-05-27 23:20:20 +0530

    pushing serializable schema

--- develop/src/interfaces/serializable.js
+++ develop/src/interfaces/serializable.js
@@ -1,6 +1,13 @@
  /*
+  * Serializability of a class is enabled by the class implementing the
+  * Serializable interface.
   * @author  Dip Bhattacharjee
   * @since   1.0
  */
 class Serializable {
+  constructor () {
+    this.config = {
+      type: 'interface'
+    }
+  }
 }
